3ee8a6517799f0c74052c05752a2e759e3eaa3a2
Italo20 <48218066+jhanziel@users.noreply.github.com>
2021-01-25 18:51:04 -0400

    V1.0

---
+++ .bash_history
@@ -0,0 +1,12 @@
+reload
+-su
+nmp megadb
+npm megadb
+npm megadb
+npm megadb
+megadb
+npm i megadb
+npm i weez
+reload
+refresh
+npm install quick.db --save
---
+++ config.js
@@ -0,0 +1,9 @@
+let config = {
+  token: "NzU1NDEzNDkwNzMwMTM5Njc4.X2C7ow.AaWjeMKBbXFaErugwgpaRyi86-M", // Reemplaze esto con el token de tu de bot.
+  prefix: "+", // El prefix de su bot para indentificar sus comandos.
+
+}
+
+// Aqui exportamos el objeto config:
+module.exports = config;
+
